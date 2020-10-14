f69b1a4d7d6659ef1adec951ff2c9dd01bdfd30e
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2020-10-14 22:57:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+namespace LINQ_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] age = { 12, 23, 21, 22, 45, 33, 20, 26, 55, 66 };
+            //var a = from i in age
+            //        where i > 20
+            //        orderby i descending
+            //        select i;
+
+            string[] names = { "Devesh", "red", "Batman", "Tim" };
+            var a = from name in names
+                    where name.Contains("e")
+                    select name;
+            //Case senstive hai
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
