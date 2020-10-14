52662bb52d898be9981c11b7d21c232e0a4d66bc
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2020-10-14 22:58:45 +0530

    Update and rename Program.cs to Contains.cs

--- Contains.cs
+++ Contains.cs
@@ -6,17 +6,13 @@ namespace LINQ_Demo
     {
         static void Main(string[] args)
         {
-            //int[] age = { 12, 23, 21, 22, 45, 33, 20, 26, 55, 66 };
-            //var a = from i in age
-            //        where i > 20
-            //        orderby i descending
-            //        select i;
-
+            
+//Case senstive hai
             string[] names = { "Devesh", "red", "Batman", "Tim" };
             var a = from name in names
                     where name.Contains("e")
                     select name;
-            //Case senstive hai
+            
             foreach (var item in a)
             {
                 Console.WriteLine(item);
