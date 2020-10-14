e25455ce934f7ac83294de8bc5df4fc965a373c8
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2020-10-14 19:01:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace LINQ_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] age = {12,23,21,22,45,33,20,26,55,66 };
+            var a = from i in age
+                    where i > 20
+                    orderby i
+                    select i;
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
